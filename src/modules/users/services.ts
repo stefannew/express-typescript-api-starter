ffd5cb1cc9ea51c438927c345b7f960f9d8d457d
Stefan New <stfnnew@gmail.com>
2020-10-16 07:51:46 -0600

    small changes

--- src/modules/users/services.ts
+++ src/modules/users/services.ts
@@ -1,15 +1,12 @@
-import { Service } from '../../core/types';
 import { createService } from '../../core/utils';
-
-type UserDTO = { firstName: string, lastName: string, email: string };
-type User = { name: string, email: string };
+import { User, UserDTO } from './types';
 
 const getUsersRequester = () => Promise.resolve([
   { firstName: 'Test', lastName: 'User', email: 'test@user.com' },
   { firstName: 'Other', lastName: 'User', email: 'other@user.com' }
 ]);
 
-const getUsersTransformer = (users) => users.map(user => ({
+const getUsersTransformer = (users: UserDTO[]) => users.map(user => ({
   name: `${user.firstName} ${user.lastName}`,
   email: user.email
 }));
---
+++ src/modules/users/types.ts
@@ -0,0 +1,2 @@
+export type UserDTO = { firstName: string, lastName: string, email: string };
+export type User = { name: string, email: string };
